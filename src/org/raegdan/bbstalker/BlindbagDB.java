a815ea55e5ec9e4d64e94fbb3fa40b1dc339b7a1
Raegdan <raegdan@gmail.com>
2013-08-10 13:37:28 +0400

    huge refactoring done

---
+++ src/org/raegdan/bbstalker/BlindbagDB.java
@@ -0,0 +1,217 @@
+package org.raegdan.bbstalker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
+
+class RegexpField
+{
+	String regexp;
+	String field;
+}
+
+class Wave
+{
+	List<RegexpField> priorities;
+	String waveid;
+	
+	Wave()
+	{
+		priorities = new ArrayList<RegexpField>();
+	}
+}
+
+class Blindbag
+{
+	List<String> bbids;
+	String waveid;
+	String name;
+	String uniqid;
+	
+	Blindbag()
+	{
+		bbids = new ArrayList<String>();
+	}
+}
+
+class BlindbagDB extends Activity {
+	List<Wave> waves;
+	List<Blindbag> blindbags;
+	
+	BlindbagDB() {
+		super();
+		waves = new ArrayList<Wave>();
+		blindbags = new ArrayList<Blindbag>();
+	}
+	
+	protected String StreamToString(InputStream is) throws IOException {
+		StringBuilder out = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(is));
+		for(String line = br.readLine(); line != null; line = br.readLine()) 
+		{
+			out.append(line);
+		}
+		br.close();
+		return out.toString();
+	}
+	
+	protected Boolean MatchRegexp(String regexp, String s)
+	{
+		return Pattern.compile(regexp).matcher(s).matches();
+	}
+	  
+	protected JSONObject GetDB(String JSONFile, Context context) throws JSONException, IOException
+	{
+		AssetManager am = context.getAssets();
+	  	InputStream is = am.open("database.json");
+	  	JSONObject DB = new JSONObject(StreamToString(is));
+	  	is.close();
+	  	return DB;
+	}
+	
+	protected void ParseDB(JSONObject DB) throws JSONException
+	{
+		for (int i = 0; i < DB.getJSONArray("waves").length(); i++)
+		{
+			Wave w = new Wave();
+			w.waveid = DB.getJSONArray("waves").getJSONObject(i).getString("waveid");
+
+			RegexpField rf = new RegexpField();
+			for (int j = 0; j < DB.getJSONArray("waves").getJSONObject(i).getJSONArray("priorities").length(); j++)
+			{
+				rf.field =  DB.getJSONArray("waves").getJSONObject(i).getJSONArray("priorities").getJSONObject(j).getString("field");
+				rf.regexp =  DB.getJSONArray("waves").getJSONObject(i).getJSONArray("priorities").getJSONObject(j).getString("regexp");
+				w.priorities.add(rf);
+			}
+			
+			waves.add(w);
+		}
+		
+		for (int i = 0; i < DB.getJSONArray("blindbags").length(); i++)
+		{
+			Blindbag bb = new Blindbag();
+			bb.name = DB.getJSONArray("blindbags").getJSONObject(i).getString("name");
+			bb.uniqid = DB.getJSONArray("blindbags").getJSONObject(i).getString("uniqid");
+			bb.waveid = DB.getJSONArray("blindbags").getJSONObject(i).getString("waveid");
+			
+			for (int j = 0; j < DB.getJSONArray("blindbags").getJSONObject(i).getJSONArray("bbids").length(); j++)
+			{
+				bb.bbids.add(DB.getJSONArray("blindbags").getJSONObject(i).getJSONArray("bbids").getString(j));
+			}
+			
+			blindbags.add(bb);
+		}
+	}
+	
+	Wave GetWaveInfo(String waveid)
+	{
+		Wave w = new Wave();
+		
+		for (int i = 0; i < waves.size(); i++)
+		{
+			if (waves.get(i).waveid.equalsIgnoreCase(waveid))
+			{
+				w = waves.get(i);
+			}
+		}
+		
+		return w;
+	}
+	
+	Boolean LoadDB(String BBStalkerDatabaseJSONFile, Context context)
+	{
+		Boolean success = true;
+		
+		try {
+			ParseDB(GetDB(BBStalkerDatabaseJSONFile, context));
+		} catch (JSONException e) {
+			e.printStackTrace();
+			success = false;
+		} catch (IOException e) {
+			e.printStackTrace();
+			success = false;
+		}
+		
+		return success;
+	}
+	
+	BlindbagDB LookupDB(String query)
+	{
+		BlindbagDB OutDB = new BlindbagDB();
+		BlindbagDB AddDB = new BlindbagDB();
+		
+		Boolean MatchMain, MatchAdd;
+		
+		for (int i = 0; i < blindbags.size(); i++)
+		{
+			MatchMain = MatchAdd = false;
+			
+			Blindbag bb = blindbags.get(i);
+			
+			Wave w = new Wave();
+			w = GetWaveInfo(bb.waveid);
+			
+			String PriorityField = "false";
+			for (int j = 0; j < w.priorities.size(); j++)
+			{
+				if (MatchRegexp(w.priorities.get(j).regexp, query))
+				{
+					PriorityField = w.priorities.get(j).field;
+				}
+			}
+				
+			for (int j = 0; j < bb.bbids.size(); j++)
+			{
+				if (bb.bbids.get(j).contains(query))
+				{
+					if (PriorityField.equalsIgnoreCase("bbids"))
+					{
+						MatchMain = true;
+					} else {
+						MatchAdd = true;
+					}
+				}
+			}
+			if (bb.name.toUpperCase(Locale.ENGLISH).contains(query.toUpperCase(Locale.ENGLISH)))
+			{
+				if (PriorityField.equalsIgnoreCase("name"))
+				{
+					MatchMain = true;
+				} else {
+					MatchAdd = true;
+				}
+			}
+			
+			if (MatchMain)
+			{
+				OutDB.blindbags.add(bb);
+
+			} else if (MatchAdd)
+			{
+				AddDB.blindbags.add(bb);
+			}
+
+			if (!OutDB.waves.contains(w))
+			{
+				OutDB.waves.add(w);
+			}
+		}
+		  
+		OutDB.blindbags.addAll(AddDB.blindbags);
+		
+		return OutDB;
+		
+	}
+}
--- src/org/raegdan/bbstalker/DBListActivity.java
+++ src/org/raegdan/bbstalker/DBListActivity.java
@@ -1,48 +1,39 @@
 package org.raegdan.bbstalker;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Locale;
-import java.util.regex.Pattern;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
+import android.view.View;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class DBListActivity extends Activity {
 	
-	  JSONObject BBDB;
 	  ListView lvDBList;
 	  TextView tvHeader;
-	  
-	  	  
+	    	  
 	  @Override
 	  protected void onCreate(Bundle savedInstanceState) {
 		  super.onCreate(savedInstanceState);
 		  setContentView(R.layout.dblist);
 		  
-		  ListView lvBDList = (ListView) findViewById(R.id.lvDBList);
+		  ListView lvDBList = (ListView) findViewById(R.id.lvDBList);
 		  TextView tvHeader = (TextView) findViewById(R.id.tvHeader);
-		  		  
-		  InitializeDB();
 		  
+		  BlindbagDB database = new BlindbagDB();
+		  if (!database.LoadDB("database.json", this))
+		  {
+			  tvHeader.setText("Error loading JSON database: I/O error or invalid JSON. If you didn't modify, rename or delete the original «database.json» file, please contact me (Raegdan)");
+			  return;
+		  }
+		  		  
 		  Intent intent = getIntent();
 		  String query = intent.getStringExtra("query");
 		  
@@ -53,158 +44,41 @@ public class DBListActivity extends Activity {
 			  tvHeader.setText("Results for «" + query + "»");
 		  }
 		  
-		  JSONObject DBResult = new JSONObject();
+		  BlindbagDB QueryResult = database.LookupDB(query);
 		  
-		  try {
-			DBResult = QueryDB(query);
-			Log.d("DBREsult", DBResult.toString(4));
-		  } catch (JSONException e) {
-			e.printStackTrace();
-		  }
-		  
-		  List<HashMap<String, Object>> DBList = new ArrayList<HashMap<String, Object>>();
-		  
-		  try {
-			  for (int i = 0; i < DBResult.getJSONArray("main").length(); i++)
+		  final List<HashMap<String, Object>> DBList = new ArrayList<HashMap<String, Object>>();
+		  for (int i = 0; i < QueryResult.blindbags.size(); i++)
+		  {
+			  String bbids = "";
+			  for (int j = 0; j < QueryResult.blindbags.get(i).bbids.size(); j++)
 			  {
-				  String bbids = "";
-				  for (int j = 0; j < DBResult.getJSONArray("main").getJSONObject(i).getJSONArray("bbids").length(); j++)
+				  bbids += QueryResult.blindbags.get(i).bbids.get(j);
+				  if (j < QueryResult.blindbags.get(i).bbids.size() - 1)
 				  {
-					  bbids += DBResult.getJSONArray("main").getJSONObject(i).getJSONArray("bbids").getString(j);
-					  if (j < DBResult.getJSONArray("main").getJSONObject(i).getJSONArray("bbids").length() - 1)
-					  {
-						  bbids += " / ";
-					  }
+					  bbids += " / ";
 				  }
-				  
-				  Resources mRes = this.getResources();
-				  Integer wavepic = mRes.getIdentifier("w" + DBResult.getJSONArray("main").getJSONObject(i).getString("waveid"), "drawable", this.getPackageName());
-
-				  HashMap<String, Object> hm = new HashMap<String, Object>();
-				  hm.put("name", DBResult.getJSONArray("main").getJSONObject(i).getString("name"));
-				  hm.put("misc", "Code: " + bbids);
-				  hm.put("img1", wavepic);
-				  DBList.add(hm);
-				  
 			  }
-		  } catch (JSONException e) {
-			  // TODO Auto-generated catch block
-			  e.printStackTrace();
+			  
+			  Resources mRes = this.getResources();
+			  Integer wavepic = mRes.getIdentifier("w" + QueryResult.blindbags.get(i).waveid, "drawable", this.getPackageName());
+			  HashMap<String, Object> hmDBList = new HashMap<String, Object>();
+			  hmDBList.put("name", QueryResult.blindbags.get(i).name);
+			  hmDBList.put("misc", "Code: " + bbids);
+			  hmDBList.put("img1", wavepic);
+			  hmDBList.put("uniqid", QueryResult.blindbags.get(i).uniqid);
+			  DBList.add(hmDBList);
 		  }
+
+
+		  lvDBList.setOnItemClickListener(new OnItemClickListener() {
+			    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+			    {
+					  DBList.get(position).get("uniqid").toString();					  
+			    }
+		  });
 		  
 		  SimpleAdapter adapter = new SimpleAdapter(this, DBList, R.layout.lvdblist, new String[] {"name", "misc", "img1"}, new int[] {R.id.lvdblist_text1, R.id.lvdblist_text2, R.id.lvdblist_img1});
-		  lvBDList.setAdapter(adapter);
-	  }
-	  
-	  protected void ShowToast(String text)
-	  {
-		  Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT); 
-		  toast.show(); 
-	  }
-	  
-	  protected Boolean MatchRegexp(String regexp, String s)
-	  {
-		  return Pattern.compile(regexp).matcher(s).matches();
-	  }
-	  
-	  protected String StreamToString(InputStream is) throws IOException {
-		  StringBuilder out = new StringBuilder();
-		  BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		  for(String line = br.readLine(); line != null; line = br.readLine()) 
-		    out.append(line);
-		  br.close();
-		  return out.toString();
-		}
-	  
-	  protected String ReadDB(Activity activity)
-	  {
-	    AssetManager am = activity.getAssets();
-	    String s = "";
-	  	try {
-		  	InputStream is = am.open("database.json");
-		  	s = StreamToString(is);
-		  	is.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	  	return s;
-	  }
-	  
-	  protected void InitializeDB()
-	  {
-
-			try {
-				BBDB = new JSONObject(ReadDB(this));
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-	  }
-	  
-	  protected JSONObject QueryDB(String query) throws JSONException
-	  {
-		JSONObject OutDB = new JSONObject();
-		JSONArray MainDB, AddDB;
-		MainDB = new JSONArray();
-		AddDB = new JSONArray();
-		
-		Boolean MatchMain, MatchAdd;
-		String Priority;
-		
-		for (int i = 0; i < BBDB.getJSONArray("blindbags").length(); i++)
-		{
-			MatchMain = MatchAdd = false;
-			
-			JSONObject record = BBDB.getJSONArray("blindbags").getJSONObject(i);
-			
-			JSONArray priorities = BBDB.getJSONObject("waves").getJSONObject(record.getString("waveid")).getJSONArray("priority");
-			Priority = "false";
-			for (int j = 0; j < priorities.length(); j++)
-			{
-				if (MatchRegexp(priorities.getJSONObject(j).getString("regexp"), query))
-				{
-					Priority = priorities.getJSONObject(j).getString("field");
-				}
-			}
-				
-			JSONArray bbids = record.getJSONArray("bbids");
-			for (int j = 0; j < bbids.length(); j++)
-			{
-				if (bbids.getString(j).contains(query))
-				{
-					if (Priority.equalsIgnoreCase("bbids"))
-					{
-						MatchMain = true;
-					} else {
-						MatchAdd = true;
-					}
-				}
-			}
-			if (record.getString("name").toUpperCase(Locale.ENGLISH).contains(query.toUpperCase(Locale.ENGLISH)))
-			{
-				if (Priority.equalsIgnoreCase("name"))
-				{
-					MatchMain = true;
-				} else {
-					MatchAdd = true;
-				}
-			}
-			
-			if (MatchMain)
-			{
-				MainDB.put(record);
-				continue;
-			} else if (MatchAdd)
-			{
-				AddDB.put(record);
-			}
-		}
+		  lvDBList.setAdapter(adapter);
 		  
-		OutDB.put("main", MainDB);
-		OutDB.put("additional", AddDB);
-		
-		return OutDB;		  
 	  }
-	  
-	  
 }
